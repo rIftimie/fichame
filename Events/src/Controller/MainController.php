3e3817c31c2d5c56476e5378de6eb01b28b7c700
RomenJ <justiciaterapeutica@gmail.com>
2023-02-08 12:36:36 +0100

    modificacion de task controller y vista de task

--- Events/src/Controller/MainController.php
+++ Events/src/Controller/MainController.php
@@ -5,15 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TaskRepository;
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
+    #[Route('/', name: 'app_main')]
     public function index(): Response
     {
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
             
-           
             
         ]);
     }
--- Events/src/Controller/TaskController.php
+++ Events/src/Controller/TaskController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Repository\TaskRepository;
 class TaskController extends AbstractController
 {
     #[Route('/task', name: 'app_task')]
-    public function index(): Response
+    public function index(TaskRepository $taskRepository): Response
     {
+  
+        
         return $this->render('task/index.html.twig', [
             'controller_name' => 'TaskController',
+            'tasks' => $taskRepository->findAll()
+            
         ]);
     }
 }
